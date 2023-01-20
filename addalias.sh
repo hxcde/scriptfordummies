e47b3667919e48fcf74479c91851c53947ddf1de
hxcde <30338980+hxcde@users.noreply.github.com>
2023-01-20 11:06:44 +0100

    Update addalias.sh

--- addalias.sh
+++ addalias.sh
@@ -2,5 +2,5 @@ echo "Which command do you want to use as alias?"
 read alias
 echo "Which command should your alias execute?"
 read command
-echo $alias="command"  | tee -a ~/.bashrc
+echo $alias="$command"  | tee -a ~/.bashrc
 echo Alias $alias added
